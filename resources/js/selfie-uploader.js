897edd5b66fd1624f5ca9b0aef82f9af390b45c0
Vkrvm <mahakram35@gmail.com>
2026-05-22 22:09:24 +0300

    fix: increase camera resolution and use lossless PNG for better AI face recognition

--- resources/js/selfie-uploader.js
+++ resources/js/selfie-uploader.js
@@ -211,7 +211,7 @@ function initSelfieUploader(root) {
         try {
             stopCamera();
             stream = await navigator.mediaDevices.getUserMedia({
-                video: { facingMode: 'user', width: { ideal: 1280 }, height: { ideal: 1280 } },
+                video: { facingMode: 'user', width: { ideal: 3840 }, height: { ideal: 3840 } },
                 audio: false,
             });
             elements.video.srcObject = stream;
@@ -238,8 +238,8 @@ function initSelfieUploader(root) {
                 return;
             }
 
-            uploadFile(new File([blob], 'selfie.jpg', { type: 'image/jpeg' }));
-        }, 'image/jpeg', 0.92);
+            uploadFile(new File([blob], 'selfie.png', { type: 'image/png' }));
+        }, 'image/png');
     });
 
     elements.cancelCamera?.addEventListener('click', () => {
